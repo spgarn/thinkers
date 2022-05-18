26193fa6276c9fdfa950692e261d5129bfa18a19
simongartner-ourliving <simon.gartner@ourliving.se>
2022-05-18 23:24:54 +0200

    testdata

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,5 @@
-export const data = [{ state: 1, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
-{ state: 2, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
-{ state: 3, answer: 'Duk', question: 'Vad blir blötare och blötare ju mer du torkar' },
-{ state: 4, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' }]
+export const data =
+    [{ state: 1, answer: 'And', question: 'Vad är fågel i England och i Sverige?' },
+    { state: 2, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
+    { state: 3, answer: 'Löftet', question: 'Vad kan man bryta utan att röra vid det?' },
+    { state: 4, answer: 'Degar', question: 'Vad gör en bagare?' }]
