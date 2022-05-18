73539b7371b198901f181079b0c4bd359a0d3db5
simongartner-ourliving <simon.gartner@ourliving.se>
2022-05-18 21:57:00 +0200

    added green

--- src/components/PaperItem.js
+++ src/components/PaperItem.js
@@ -5,10 +5,13 @@ import React from 'react'
 
 
 
-const PaperItem = ({ data, setState }) => {
+const PaperItem = ({ data, setState, points }) => {
     return (
         <Grid gap={'2rem'} container>{data?.map(item => <Grid key={item.state} width={'80px'} height={'80px'} item>
-            <Paper onClick={() => setState(item.state)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', cursor: 'pointer', backgroundColor: 'lightgrey' }}>{item.state}</Paper></Grid>)}</Grid >
+            <Paper onClick={() => setState(item.state)} style={{
+                display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', cursor: 'pointer',
+                backgroundColor: [...points].includes(item.answer.toLowerCase()) ? 'lightgreen' : 'lightgray'
+            }}>{item.state}</Paper></Grid>)}</Grid >
 
     )
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
 export const data = [{ state: 1, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
 { state: 2, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
-{ state: 3, answer: 'duk', question: 'Vad blir blötare och blötare ju mer du torkar' },
+{ state: 3, answer: 'Duk', question: 'Vad blir blötare och blötare ju mer du torkar' },
 { state: 4, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' }]
