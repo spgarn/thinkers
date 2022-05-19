ad2714d3349d9c1e5d1e66861f6dfceb864d5fde
Viktor Collin <viktor@callide.se>
2022-05-19 09:21:21 +0200

    array av möjliga svar + fixat tidsjämförelse

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,9 @@
 export const data =
     [
-        { state: 1, answer: 'And', question: 'Vad är fågel i England och i Sverige?' },
-        { state: 2, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
-        { state: 3, answer: 'Löftet', question: 'Vad kan man bryta utan att röra vid det?' },
-        { state: 4, answer: 'Degar', question: 'Vad gör en bagare?' },
-        { state: 5, answer: 'Peders', question: 'B. ?' },
-        { state: 6, answer: 'not', question: 'Vad är klockan?' },
+        { state: 1, answer: ['And'], question: 'Vad är fågel i England och i Sverige?' },
+        { state: 2, answer: ['Handduk', 'Handduken'], question: 'Vad blir blötare och blötare ju mer du torkar' },
+        { state: 3, answer: ['Löftet', 'Löfte', 'Löften'], question: 'Vad kan man bryta utan att röra vid det?' },
+        { state: 4, answer: ['Degar', 'Deg'], question: 'Vad gör en bagare?' },
+        { state: 5, answer: ['Peders'], question: 'B. ?' },
+        { state: 6, answer: [], question: 'Vad är klockan?' },
     ]
