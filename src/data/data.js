325e455893c7026a54c08175554992e7a7d40fe2
simongartner-ourliving <simon.gartner@ourliving.se>
2022-05-19 08:27:53 +0200

    new questions

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,13 @@
+const hour = new Date().getHours().toString()
+const minute = new Date().getMinutes().toString()
+const time = hour + minute
+
 export const data =
-    [{ state: 1, answer: 'And', question: 'Vad är fågel i England och i Sverige?' },
-    { state: 2, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
-    { state: 3, answer: 'Löftet', question: 'Vad kan man bryta utan att röra vid det?' },
-    { state: 4, answer: 'Degar', question: 'Vad gör en bagare?' }]
+    [
+        { state: 1, answer: 'And', question: 'Vad är fågel i England och i Sverige?' },
+        { state: 2, answer: 'Handuk', question: 'Vad blir blötare och blötare ju mer du torkar' },
+        { state: 3, answer: 'Löftet', question: 'Vad kan man bryta utan att röra vid det?' },
+        { state: 4, answer: 'Degar', question: 'Vad gör en bagare?' },
+        { state: 5, answer: 'Peders', question: 'B. ?' },
+        { state: 6, answer: time, question: 'Vad är klockan?' },
+    ]
