446747e613d957f99d15a049236759f5760d954a
Simon Gärtner <simon.gartner@positiongreen.com>
2024-04-23 21:31:55 +0200

    new data

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,10 @@
 export const data =
     [
         { state: 1, answer: ['1557'], question: 'Törstiga män bör finna sin lycka på kylning i det lilla köket.' },
+        { state: 2, answer: ['noob'], question: 'Vad kännetecknar Anton?' },
+        { state: 3, answer: ['diablo'], question: 'Fellans favoritspel' },
+        { state: 4, answer: ['arne'], question: 'Vad heter Arne?' },
+        { state: 5, answer: ['Lulle'], question: 'Lukas smeknamn' },
+        { state: 6, answer: ['spg'], question: 'simon peter gärtner' },
 
     ]
